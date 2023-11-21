b25203ed753b6b5d8718186a7d96dc24735728ce
Gamecraft400 <102668085+Gamecraft400@users.noreply.github.com>
2023-11-21 15:22:17 +0000

    Ajoute de affiche_ligne

--- Affiche.java
+++ Affiche.java
@@ -1,1 +1,16 @@
+public class Affiche
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("DS R5.07");
+        Affiche.affiche_ligne();
+    }
 
+
+    public static void affiche_ligne()
+    {
+        System.out.println("*****");
+    }
+
+
+}
